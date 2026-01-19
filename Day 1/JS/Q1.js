ba661bc0216d5237c1b1ef4f77250b3f559719b4
abhishek bochare <abhishekbochare2003@gmail.com>
2026-01-19 22:48:11 +0530

    updated

--- Day 1/JS/Q1.js
+++ Day 1/JS/Q1.js
@@ -7,6 +7,26 @@ const products = [
   ["Clothing", "Jacket", 89],
 ];
 
+// let result = {};
+
+// for (let i = 0; i < products.length; i++) {
+//   let category = products[i][0];
+//   let name = products[i][1];
+//   let price = products[i][2];
+
+//   if (!result[category]) {
+//     result[category] = [];
+//   }
+
+//   result[category].push({ name, price });
+// }
+
+// for (let category in result) {
+//   result[category].sort((a, b) => a.price - b.price);
+// }
+
+// console.log(result);
+
 let result = products.reduce((obj, item) => {
   let category = item[0];
   let name = item[1];
